7413760e40581058529ecc5cc37e56801aba3213
Giovambattista Fazioli <giovambattista.fazioli@gmail.com>
2025-02-11 18:50:14 +0100

    🐛 fixes wrong import

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import { Container } from '@mantine/core';
 import { ColorSchemeToggle } from '@/components/ColorSchemeToggle/ColorSchemeToggle';
-import { Content } from '@/components/Content';
+import { Content } from '@/components/Content/Content';
 import { Welcome } from '@/components/Welcome/Welcome';
 
 export default function HomePage() {
