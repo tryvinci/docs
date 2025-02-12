296bde49f0879fad3064745d94e5e9a0639ff7d7
Giovambattista Fazioli <giovambattista.fazioli@gmail.com>
2025-02-12 11:15:50 +0100

    ✨ adds base api

---
+++ app/api/search/route.ts
@@ -0,0 +1,11 @@
+import { type NextRequest } from 'next/server';
+
+export function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams;
+  const query = searchParams.get('query');
+  // query is "hello" for /api/search?query=hello
+
+  return new Response(`Hello, ${query}!`, {
+    headers: { 'content-type': 'text/plain' },
+  });
+}
